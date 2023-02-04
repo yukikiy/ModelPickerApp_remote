c02f19e4d35aa3dd1d4f4d951dadc94345602cf6
yukikiy <kisaku0120@icloud.com>
2023-02-04 11:40:23 +0900

    Initial Commit

---
+++ ModelPickerAppUITests/ModelPickerAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ModelPickerAppUITestsLaunchTests.swift
+//  ModelPickerAppUITests
+//
+//  Created by 箕作勇輝 on 2/4/23.
+//
+
+import XCTest
+
+final class ModelPickerAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
